c0c6b4b4767a0e7b2d164d77a74efa1aaf252109
athif <athifirshad@hotmail.com>
2023-11-19 11:33:09 +0530

    Update CORS policy to allow requests from specific origin

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Origin", "https://searchrecipes.vercel.app")
         next.ServeHTTP(w, r)
     })
 }
