22d1dccc01114d2115060b7f68e1f124e3c87021
athif <athifirshad@hotmail.com>
2023-12-05 18:15:41 +0530

    created readme.md

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,8 +3,8 @@ package main
 import "net/http"
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "https://searchrecipes.vercel.app")
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*") //"https://searchrecipes.vercel.app")
+		next.ServeHTTP(w, r)
+	})
 }
